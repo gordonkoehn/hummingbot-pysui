c279c3f9275614c25a187e6c1bf7356cb64aab4d
Gordon Julian Köhn <gordon@koehn.net>
2024-05-30 21:14:56 -0500

    extract interfaces

--- hummingbot/connector/exchange/suidex/libsui/_interface.py
+++ hummingbot/connector/exchange/suidex/libsui/_interface.py
@@ -4,3 +4,5 @@ Note:
     This submodule should not import other submodules,
     so we do not introduce circular imports.
 """
+
+from hummingbot.connector.exchange.suidex.libsui._sui_client_config import cfg, client
