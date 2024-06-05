0ce21cb272d48377183d1bfacbce06643748e791
Martin Dengler <martin@martindengler.com>
2024-06-05 14:44:18 -0400

    libsui/_create_pool.py: update for better defaulting and new libsui constants

--- hummingbot/connector/exchange/suidex/libsui/_create_pool.py
+++ hummingbot/connector/exchange/suidex/libsui/_create_pool.py
@@ -1,4 +1,15 @@
+"""
+Usage:
+
+```
+$ python -m hummingbot.connector.exchange.suidex.libsui._create_pool
+
+```
+
+"""
+
 import os
+import sys
 
 from pysui import handle_result
 from pysui.sui.sui_txn import SyncTransaction
@@ -6,23 +17,29 @@ from pysui.sui.sui_types.scalars import ObjectID, SuiU64
 
 import hummingbot.connector.exchange.suidex.libsui
 
-NETWORK, CFG, CLIENT = hummingbot.connector.exchange.suidex.libsui.init()
 
-
-def create_pool(net=None):
+def create_pool(net=None, cfg=None, client=None):
     """Creates a pool with REALUSDC and SUI"""
-    net = NETWORK if net is None else net
+    if not all((net, cfg, client)):
+        NETWORK, CFG, CLIENT = hummingbot.connector.exchange.suidex.libsui.init()
+        if net is None:
+            net = NETWORK
+        if cfg is None:
+            cfg = CFG
+        if client is None:
+            client = CLIENT
+
     package_id = os.getenv(f"{net.upper()}_PACKAGE_ID")
     if package_id is None:
         raise ValueError(f"Network {net} not supported (add {net.upper()}_PACKAGE_ID to .env?)")
 
     # TODO: add case for sponsoredTransaction
-    txn = SyncTransaction(client=CLIENT)
+    txn = SyncTransaction(client=client)
 
     # TODO: get SUI coin objects using gql
     # Retrieved somehow
     # coin_to_split = "0xfa50148afdbbaadb5bd509600845c7f01001ef1dd52f8d880548aaf1444e47e1"  # noqa: mock
-    # txn.transfer_objects(transfers=[txn.split_coin(coin=coin_to_split, amounts=[1000000000])], recipient=CFG.active_address)
+    # txn.transfer_objects(transfers=[txn.split_coin(coin=coin_to_split, amounts=[1000000000])], recipient=cfg.active_address)
     # txn.split_coin(coin=coin_to_split, amounts=[1000000000])
     # gas_object = txn.split_coin_and_return(coin=coin_to_split, split_count=2)
 
@@ -36,7 +53,7 @@ def create_pool(net=None):
     )
     # txn.transfer_objects(
     #     transfers=[returned_pool],
-    #     recipient=CFG.active_address,
+    #     recipient=cfg.active_address,
     # )
     tx_result = handle_result(txn.execute(gas_budget="10000000"))
 
